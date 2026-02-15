f35818935bf3b89f368166509e479ad0396e23bc
Anony78826 <sujitm7875@gmail.com>
2026-02-15 12:48:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "8581545651:AAEVZ5pJKXDMuzL3OwloM5FHQGkZxkbQmfw"
+CHANNEL_USERNAME = "@IG_CC_SELLERS"  # example: @MyRewardsChannel
+ADMIN_ID = 1295478989  # replace with your telegram numeric id
+
+REWARD_NEW_USER = 2
+REWARD_REFERRAL = 3
+
+SEPARATOR = "---"
